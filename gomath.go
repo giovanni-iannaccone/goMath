95acd4a7f30635dab88c9c5107cdd54690b7890d
Giovanni Iannaccone <80602545+giovanni-iannaccone@users.noreply.github.com>
2023-12-29 22:54:10 +0100

    Update gomath.go

--- gomath.go
+++ gomath.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 )
 
-func removeFloat64Index(array []float64, index int) []float64 {
-	var new_array []float64
+func removeIndex(array []interface{}, index int) []interface{} {
+	var new_array []interface{}
 
 	for i := 0; i < len(array); i++ {
 		if i != index {
@@ -21,60 +21,66 @@ func removeFloat64Index(array []float64, index int) []float64 {
 	return new_array
 }
 
-func removeStringIndex(array []string, index int) []string {
-	var new_array []string
-
-	for i := 0; i < len(array); i++ {
-		if i != index {
-			new_array = append(new_array, array[i])
+func solve(nums_signs []interface{}) []interface{} {
+	fmt.Println(nums_signs)
+	for i := 0; i < len(nums_signs); {
+		fmt.Printf("IN ^\n")
+		if i+1 < len(nums_signs) {
+			if nums_signs[i] == "^" {
+				nums_signs[i-1] = math.Pow(nums_signs[i-1].(float64), nums_signs[i+1].(float64))
+				nums_signs = removeIndex(nums_signs, i)
+				nums_signs = removeIndex(nums_signs, i)
+			} else {
+				i++
+			}
+		} else {
+			i++
 		}
 	}
 
-	return new_array
-}
-
-func solve(nums []float64, signs []string) []float64 {
-	for i := 0; i < len(signs); i++ {
-		if strings.Compare(signs[i], "^") == 0 {
-			nums[i] = math.Pow(nums[i], nums[i + 1])
-			nums = removeFloat64Index(nums, i + 1)
-			signs = removeStringIndex(signs, i)
+	for i := 0; i < len(nums_signs); {
+		fmt.Printf("IN */\n")
+		if i+1 < len(nums_signs) {
+			if nums_signs[i] == "*" {
+				nums_signs[i-1] = nums_signs[i-1].(float64) * nums_signs[i+1].(float64)
+				nums_signs = removeIndex(nums_signs, i)
+				nums_signs = removeIndex(nums_signs, i)
+			} else if nums_signs[i] == "/" {
+				nums_signs[i-1] = nums_signs[i-1].(float64) / nums_signs[i+1].(float64)
+				nums_signs = removeIndex(nums_signs, i)
+				nums_signs = removeIndex(nums_signs, i)
+			} else {
+				i++
+			}
+		} else {
+			i++
 		}
 	}
 
-	for i := 0; i < len(signs); i++ {
-		if strings.Compare(signs[i], "*") == 0 {
-			nums[i] = nums[i] * nums[i + 1]
-			nums = removeFloat64Index(nums, i + 1)
-			signs = removeStringIndex(signs, i)
-
-		} else if strings.Compare(signs[i], "/") == 0 {
-			nums[i] = nums[i] / nums[i + 1]
-			nums = removeFloat64Index(nums, i + 1)
-			signs = removeStringIndex(signs, i)
+	for i := 0; i < len(nums_signs); {
+		fmt.Printf("IN +-\n")
+		if i+1 < len(nums_signs) {
+			if nums_signs[i] == "+" {
+				nums_signs[i-1] = nums_signs[i-1].(float64) + nums_signs[i+1].(float64)
+				nums_signs = removeIndex(nums_signs, i)
+				nums_signs = removeIndex(nums_signs, i)
+			} else if nums_signs[i] == "-" {
+				nums_signs[i-1] = nums_signs[i-1].(float64) - nums_signs[i+1].(float64)
+				nums_signs = removeIndex(nums_signs, i)
+				nums_signs = removeIndex(nums_signs, i)
+			} else {
+				i++
+			}
+		} else {
+			i++
 		}
 	}
 
-	for i := 0; i < len(signs); i++ {
-		if strings.Compare(signs[i], "+") == 0 {
-			nums[i] = nums[i] + nums[i + 1]
-			nums = removeFloat64Index(nums, i + 1)
-			signs = removeStringIndex(signs, i)
-
-		} else if strings.Compare(signs[i], "-") == 0 {
-			nums[i] = nums[i] - nums[i + 1]
-			nums = removeFloat64Index(nums, i + 1)
-			signs = removeStringIndex(signs, i)
-		}
-	}
-
-	return nums
+	return nums_signs
 }
 
 func main() {
-
-	var nums []float64
-	var signs []string
+	var nums_signs []interface{}
 	//var vars map[string]float64
 
 	var closed_par_idx int = -1
@@ -86,36 +92,39 @@ func main() {
 	calc = strings.TrimSpace(calc)
 	calcs := strings.Split(calc, " ")
 
-	for i := 0; i <= len(calcs) - 1; i++ {
-		float_num, err := strconv.ParseFloat(calcs[i], 64)
+	//for i := 0; len(nums_signs) != 1; i++ {
 
-		if err != nil {
-			sign := calcs[i]
-			signs = append(signs, sign)			
+		for i := 0; i <= len(calcs)-1; i++ {
+			float_num, err := strconv.ParseFloat(calcs[i], 64)
+
+			if err == nil {
+				nums_signs = append(nums_signs, float_num)
+			} else {
+				nums_signs = append(nums_signs, calcs[i])
+			}
+		}
+
+		for i := 0; i < len(nums_signs); i++ {
+			if nums_signs[i] == "(" {
+				opened_par_idx = i
+			} else if nums_signs[i] == ")" {
+				closed_par_idx = i
+				break
+			}
+		}
+
+		if closed_par_idx < opened_par_idx {
+			fmt.Printf("Invalid expression")
+			//break
+		} else if closed_par_idx > 0 && opened_par_idx >= 0 {
+			nums_signs = removeIndex(nums_signs, opened_par_idx)
+			nums_signs = removeIndex(nums_signs, closed_par_idx - 1)
+			nums_signs[opened_par_idx] = solve(nums_signs[opened_par_idx : closed_par_idx - 1])[0]
+
 		} else {
-			nums = append(nums, float_num)
+			nums_signs = solve(nums_signs)
 		}
-	}
+//  }
 
-	for i := 0; i < len(signs) - 1; i++ {
-		if strings.Compare(signs[i], "(") == 0 {
-			opened_par_idx = i
-		} else if strings.Compare(signs[i], ")") == 0 {
-			closed_par_idx = i
-		}
-
-		if closed_par_idx > 0 {
-			break
-		}
-	}
-
-	if closed_par_idx != opened_par_idx || closed_par_idx < opened_par_idx {
-		fmt.Printf("Invalid expression")
-	} else if closed_par_idx > 0 && opened_par_idx >= 0 {
-		nums = solve(nums[opened_par_idx : closed_par_idx], signs[opened_par_idx : closed_par_idx])
-	} else {
-		nums = solve(nums, signs)
-	}
-
-	fmt.Printf("%f", nums[0])
+	fmt.Println(nums_signs)
 }
